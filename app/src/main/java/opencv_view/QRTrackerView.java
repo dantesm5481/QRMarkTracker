bb20299dc24f631dbe5dfb81ec567aaa10ad9d66
Raymond <raymondzorrofinder@gmail.com>
2016-06-27 15:12:25 +0800

    QRcode finish ，push

--- app/src/main/java/opencv_view/QRTrackerView.java
+++ app/src/main/java/opencv_view/QRTrackerView.java
@@ -1,12 +1,16 @@
 package opencv_view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Picture;
 import android.util.Log;
+import android.widget.ImageView;
 
 
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
@@ -14,6 +18,7 @@ import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
+import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
@@ -22,6 +27,7 @@ import java.util.List;
 import java.util.Vector;
 
 import opencv_view.BasicOpenCVView.BasicOpenCV;
+import qr_scanner.QRgenerate;
 
 
 /**
@@ -33,11 +39,11 @@ public class QRTrackerView extends BasicOpenCV implements CameraBridgeViewBase.C
     private ArrayList<Mat> matList;
     private ArrayList<MatOfPoint> contours  ;
 
-    //todo test
+    // final element
     private MatOfPoint temp;
     private MatOfPoint2f approxCurve;
     private int debug = 0;
-
+    private QRgenerate generate;
 
 
     public QRTrackerView(Context context, int cameraId) {
@@ -51,13 +57,15 @@ public class QRTrackerView extends BasicOpenCV implements CameraBridgeViewBase.C
 
         matResult = new Mat();
         contours = new ArrayList<MatOfPoint>();
-
         matList = new ArrayList<Mat>();
 
         //Todo test
         colormat = new Mat();
         temp = new MatOfPoint();
         approxCurve = new MatOfPoint2f();
+        //QRScan
+//        generate = new QRgenerate(getContext());
+
     }
 
     @Override
@@ -70,15 +78,14 @@ public class QRTrackerView extends BasicOpenCV implements CameraBridgeViewBase.C
     }
 
     @Override
-    public Mat onCameraFrame(Mat mat) {
-        //順序關係  mat > colormat > matCB > matResult
+    public Mat onCameraFrame(Mat mat) {//順序關係  mat > colormat > matCB > matResult
 
         Imgproc.cvtColor(mat, colormat, Imgproc.COLOR_RGB2YCrCb);
         Core.split(colormat, matList); // 分離色彩通道 Y Cr Cb 分別進入 陣列 0、1、2
         matCB = matList.get(2);
-        Imgproc.threshold(matCB, matResult, 144, 255, Imgproc.THRESH_BINARY);  //只取 CB值
+        Imgproc.threshold(matCB, matResult, 130, 255, Imgproc.THRESH_BINARY);  //只取 CB值
 
-        //提取四邊
+        //todo 提取四邊
         Imgproc.findContours(matResult, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE,new Point());
         SquareContours();
 //        Imgproc.Canny(matCB,matResult,80,120);  //邊緣檢測模式
@@ -86,7 +93,7 @@ public class QRTrackerView extends BasicOpenCV implements CameraBridgeViewBase.C
 
 
 //        return mat;
-        return matResult;
+        return matCB;
     }
 
     private void SquareContours() {
@@ -97,15 +104,36 @@ public class QRTrackerView extends BasicOpenCV implements CameraBridgeViewBase.C
                 temp = contours.get(i);
                 MatOfPoint2f new_mat = new MatOfPoint2f(temp.toArray());
                 int contourSize = (int) temp.total();
-                Imgproc.approxPolyDP(new_mat, approxCurve, contourSize * 0.05, true);
+                Imgproc.approxPolyDP(new_mat, approxCurve , contourSize * 0.05, true);
                 if (approxCurve.total() == 4) { //取得四點
                     if (Imgproc.isContourConvex(temp)) {//判斷是否為多邊形
-                        Log.v(" Raymond ", "  : " + " find the QRsquare"+String.valueOf(debug++));
+                        Log.v(" Raymond ", "  : " + " find the QRsquare" + String.valueOf(debug++));
                         Imgproc.drawContours(matResult, contours, i, new Scalar(0, 255, 0), 8);
-
+                        getMat2Image();
                     }
                 }}}
     }
+
+    private Bitmap getMat2Image(){
+
+        Bitmap bm = Bitmap.createBitmap(matResult.cols(), matResult.rows(),Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(matResult , bm);// Mat convert to Bitmap
+
+
+        return bm;
+    }
+
+
+    private ImageView setImageViewonMat(){
+
+        ImageView result = new ImageView(getContext());
+
+        return result;
+
+    }
+
+
+
 
 
     //QR Tracker//todo JACK way
---
+++ app/src/main/java/qr_scanner/QRgenerate.java
@@ -0,0 +1,19 @@
+package qr_scanner;
+
+import android.app.Activity;
+
+/**
+ * Created by Raymond Zorro on 16/6/27.
+ */
+public class QRgenerate {
+
+
+    Activity activity;
+
+    public QRgenerate(Activity activity) {
+        this.activity = activity;
+
+    }
+
+    public static final String result(String result){return result;}
+}
